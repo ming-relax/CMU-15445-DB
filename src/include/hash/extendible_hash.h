e9b51af9620ee9f0b7e4a0bc1883a2730d8b2863
huming <humings@gmail.com>
2019-07-12 10:03:28 +0800

    reformat code

--- src/include/hash/extendible_hash.h
+++ src/include/hash/extendible_hash.h
@@ -18,44 +18,44 @@
 
 namespace cmudb {
 
-		template<typename K, typename V>
-		class ExtendibleHash : public HashTable<K, V> {
-		public:
-				// constructor
-				ExtendibleHash(size_t size);
+template<typename K, typename V>
+class ExtendibleHash : public HashTable<K, V> {
+ public:
+  // constructor
+  ExtendibleHash(size_t size);
 
-				// helper function to generate hash addressing
-				size_t HashKey(const K &key);
+  // helper function to generate hash addressing
+  size_t HashKey(const K &key);
 
-				// helper function to get global & local depth
-				int GetGlobalDepth() const;
+  // helper function to get global & local depth
+  int GetGlobalDepth() const;
 
-				int GetLocalDepth(int bucket_id) const;
+  int GetLocalDepth(int bucket_id) const;
 
-				int GetNumBuckets() const;
+  int GetNumBuckets() const;
 
-				// lookup and modifier
-				bool Find(const K &key, V &value) override;
+  // lookup and modifier
+  bool Find(const K &key, V &value) override;
 
-				bool Remove(const K &key) override;
+  bool Remove(const K &key) override;
 
-				void Insert(const K &key, const V &value) override;
+  void Insert(const K &key, const V &value) override;
 
-		private:
-				// add your own member variables here
-				const size_t bitNum = sizeof(size_t) * 8;
-				const size_t bucketSize;
-				class Bucket {
-				public:
-						std::vector<std::pair<K, V>> kvs;
-						size_t freeCnt;
-						int localDepth;
-						Bucket(size_t size, int localDepth): freeCnt(size), localDepth(localDepth) {}
-				};
-				int globalDepth;
-				std::vector<Bucket *> bucketList;
-				std::mutex mtx;
+ private:
+  // add your own member variables here
+  const size_t bitNum = sizeof(size_t) * 8;
+  const size_t bucketSize;
+  class Bucket {
+   public:
+    std::vector<std::pair<K, V>> kvs;
+    size_t freeCnt;
+    int localDepth;
+    Bucket(size_t size, int localDepth) : freeCnt(size), localDepth(localDepth) {}
+  };
+  int globalDepth;
+  std::vector<Bucket *> bucketList;
+  std::mutex mtx;
 
-				void InsertIntoBucket(Bucket * bucket, const K &key, const V &value);
-		};
+  void InsertIntoBucket(Bucket *bucket, const K &key, const V &value);
+};
 } // namespace cmudb
